e3970989a9dd88dbf1cefc0d7823ab9b28198db6
PAUSE <Callumdavid4664@gmail.com>
2026-05-12 13:08:06 +0100

    A whole lot of stuff...

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const text = document.querySelector(".scroll-text")
+
+window.addEventListener("scroll", () => {
+  let y = window.scrollY
+  let move = Math.min(y * 0.5, 150)
+  text.style.transform = "translateY(-" + move + "px)"
+})
+
+const btn = document.getElementById("modeToggle")
+
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("light")
+  document.body.classList.toggle("dark")
+})
+
+const burger = document.getElementById("burger")
+const nav = document.getElementById("navLinks")
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active")
+  nav.classList.toggle("active")
+})
